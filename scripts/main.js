fdb3c2c592c94e0fe951670d4775336f91a3c11b
Phạm Quang Khang <khang21081995@gmail.com>
2016-06-29 23:42:43 +0700

    Copy Mr Ton Hong Duc

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,38 @@
+var TankOnline = {};
+
 window.onload = function(){
-  var canvas = document.createElement("canvas");
-  document.body.appendChild(canvas);
+  TankOnline.game = new Phaser.Game(window.innerWidth,
+                                    window.innerHeight,
+                                    Phaser.AUTO,
+                                    '',
+                                    { preload: preload, create: create, update: update });
+}
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+var tank, tank2;
+var preload = function(){
+  TankOnline.game.load.image('tankDown', './images/tank_player1_down_c0_t1_s1.png');
+  TankOnline.game.load.image('tankUp', './images/tank_player1_up_c0_t1_s1.png');
+  TankOnline.game.load.image('tankLeft', './images/tank_player1_left_c0_t1_s1.png');
+  TankOnline.game.load.image('tankRight', './images/tank_player1_right_c0_t1_s1.png');
+}
 
-  context = canvas.getContext("2d");
+var create = function(){
+  TankOnline.game.physics.startSystem(Phaser.Physics.ARCADE);
+  TankOnline.keyboard = TankOnline.game.input.keyboard;
 
+  tank = new Tank(window.innerWidth/2, window.innerHeight/2);
+  tank2 = new Tank(window.innerWidth/2 - 200, window.innerHeight/2);
+}
+var update = function(){
+  var directionX, directionY;
+  if(TankOnline.keyboard.isDown(Phaser.KeyCode.LEFT)) directionX = -1;
+  else if (TankOnline.keyboard.isDown(Phaser.KeyCode.RIGHT)) directionX = 1;
+  else directionX = 0;
+
+  if(TankOnline.keyboard.isDown(Phaser.KeyCode.UP)) directionY = -1;
+  else if (TankOnline.keyboard.isDown(Phaser.KeyCode.DOWN)) directionY = 1;
+  else directionY = 0;
+
+  tank.update(directionX, directionY);
+  tank2.update(directionX, directionY);
 }
