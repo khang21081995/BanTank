3a536037fdc378b45007dfc151115d60f947c486
Phạm Quang Khang <khang21081995@gmail.com>
2016-06-29 22:59:14 +0700

    initial Game

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+  var canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  context = canvas.getContext("2d");
+
+}
